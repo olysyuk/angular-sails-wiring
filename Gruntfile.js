1b58ed4c796434b3d25a1ca3c20d4391901cc4df
Oleksandr Lysyuk <olysyuk@gmail.com>
2015-05-30 13:01:06 +0300

    Serving data from frontend and backend

---
+++ Gruntfile.js
@@ -0,0 +1,144 @@
+'use strict';
+
+module.exports = function (grunt) {
+    // Load grunt tasks automatically
+    require('load-grunt-tasks')(grunt);
+
+
+    var wiringConfig = {   
+        port: 3000,
+
+        backend: {
+            dir: 'backend',
+            port: 1338
+        },
+
+        frontend: {
+            dir: 'frontend',
+            port: 9000
+        },
+
+        distFolder: 'distFolder'
+    };
+    
+    grunt.initConfig({
+        // Configuration about how frontend and backend should be sticked together
+        wiring: wiringConfig,
+
+        nodemon: { // Running backend server
+            serveBackend: {
+                script: 'app.js',
+                options: {
+                    cwd: __dirname + '/' + '<%= wiring.backend.dir %>',
+                    env: {
+                        PORT: '<%= wiring.backend.port %>'
+                    }
+                }
+            }
+        },
+
+        subgrunt: { // Running frontend watch tasks
+            serveFrontend: {    
+                frontend: 'serve'
+            },
+            buildFrontend: {
+                frontend: 'build'
+            }
+        },
+
+        connect: { // TODO add livereload
+            serve: {
+                options: {
+                    open: true,
+                    port: '<%=wiring.port %>',
+                    hostname: 'localhost',
+                    keepalive : true,
+                    middleware: function (connect, options, defaultMiddleware) {
+                        var proxyUtils = require('grunt-connect-proxy/lib/utils'),
+                            frontendProxy = proxyUtils.proxies()[0],
+                            backendProxy = proxyUtils.proxies()[1];
+                        
+                        // Checks if frontend file exists, on 404 passes
+                        var frontendHandler = function(req, res, next) {
+                            var fakeRes = require('node-mocks-http').createResponse({
+                                eventEmitter: require('events').EventEmitter
+                            });
+
+                            fakeRes.on('end', function() {
+                                if (fakeRes.statusCode === 404) {
+                                    next();
+                                } else {
+                                    frontendProxy.server.web(req, res);
+                                }
+                            });
+
+                            frontendProxy.server.web(req, fakeRes);
+                            frontendProxy.server.on('error', function(error) {
+                                if (!res.headersSent) {
+                                    res.setHeader('Content-type', 'text/html');
+                                    res.end('Wait until frontend and backend servers are up!'+
+                                        '<script type="text/javascript">'+
+                                        'setTimeout(function(){ window.location.reload(1); }, 2000);'+
+                                        '</script>'
+                                    );
+                                }
+                            });
+                        };
+
+                        // Proxies request to backend
+                        var backendHandler = function(req, res, next) {
+                            backendProxy.server.web(req, res);
+                        }
+                       
+                        return [
+                            frontendHandler,
+                            backendHandler
+                        ].concat(defaultMiddleware);
+                    }
+                },
+                proxies: [{
+                    context: '/',
+                    host: 'localhost',
+                    port: '<%= wiring.frontend.port %>'
+                },
+                {
+                    context: '/',
+                    host: 'localhost',
+                    port: '<%= wiring.backend.port %>'
+                }]
+            },
+        },
+
+        concurrent: { // Allows nodemon(backend) and watch(frontend) in single terminal window
+            serve: { 
+                tasks: [
+                    'nodemon:serveBackend', 
+                    'subgrunt:serveFrontend', 
+                    'wiringServe'
+                ],
+                options: {
+                      logConcurrentOutput: true
+                }
+            }
+        },
+    });
+
+    grunt.registerTask('wiringServe', [
+        'configureProxies:serve',
+        'connect:serve'
+    ]);
+
+    grunt.registerTask('serve', 'Compile and start frontend with livereload and backend with watch', [
+        'concurrent:serve'
+    ]);
+
+
+    grunt.registerTask('build', 'Build frontend, build backend and link things together', [
+        'subgrunt:buildFrontend'
+    ]);
+    
+
+    // Run backend with nodemon 
+    // Watch frontend with grunt-contrib-watch
+    // Disable sails hooks and assets compilation
+};
--- README.md
+++ README.md
@@ -1,2 +1,74 @@
 # angular-sails-wiring
-Grunt based config to keep frontend and backend separate
+Angular and sails application boilerplate. Grunt.js based config to keep frontend and backend separate and maintainable.
+
+Most solutions like [sails-generate-frontend-angular](https://github.com/chiefy/sails-generate-frontend-angular) and [simple-angular-rails-app](https://github.com/EmmanuelOga/simple-angular-rails-app) are either putting frontend inside of backend folder or backend inside frontend. Such approach leads to mixup of frontend and backend files and limits generators usage.
+
+# Project sturcutre
+Having separate frontend and backend folders makes project structure clear. They are being wired together by upper-level grunt file. 
+
+frontend/  
+--> app/ (raw source)  
+--> dist/ (compiled source)  
+--> Gruntfile.js (frontend: config with tasks 'watch', 'serve' and 'build')  
+
+backend/  
+--> api/ (raw source)  
+--> assets/ (folder for compiled frontend source)  
+--> Gruntfile.js (backend: config with tasks 'watch', 'serve' and 'build' )  
+
+
+Gruntfile (subgrunt that relies on upper files, and links frontend/dist to backend/assets when needed)
+
+
+# Set-up
+You are free to choose what generator use for both frontend and backend
+
+## 1. Frontend (Angularjs)
+One of the ways to generate angularjs app is [yeoman angular generator](https://github.com/yeoman/generator-angular), but you are not limited to it. Run the following command after installation complete.
+
+```
+cd frontend && yo angular && cd ..
+```
+It will create AngularJS app in frontend folder. It's assumed that you build is configured to frontend/dist folder (default setting in yeoman generator). You can use generator to add more models to it.
+
+## 2. Backend (Sails)
+Create new SailsJS application in your backend folder. 
+```
+cd backend
+rm .gitkeep && sails new && cd ..
+```
+
+## 3. Check config
+angular-sails-wiring sets up a new connect server at port 3000. It proxies requests to frontend and backend parts (checks frontend for 404 and falls back to backend).
+
+```
+	var wiringConfig = {   
+        port: 3000,
+
+        backend: {
+            dir: 'backend',
+            port: 1338
+        },
+
+        frontend: {
+            dir: 'frontend',
+            port: 9000
+        },
+
+        distFolder: 'distFolder'
+    };
+```
+
+## 4. Start development
+
+```
+npm install
+grunt serve
+```
+
+It is recommended to change ```open: true``` to ```open: false``` on frontend/Gruntfile.js, it will prevent extra window (localhost:9000 on start)
+
+## 5. Deploy
+```
+grunt build
+```
